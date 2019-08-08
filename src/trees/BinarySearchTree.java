8979f61067d6f1c6d8cec236c295bfe022420631
sibnaray <sibinarayanan02@gmail.com>
2019-08-07 22:35:34 -0700

    T

--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -38,6 +38,44 @@ public class BinarySearchTree {
         return root;
     }
 
+    // This method mainly calls deleteRec()
+    void delete( int data){
+        root = deleteRec(root,data);
+    }
+
+    Node deleteRec(Node root, int data){
+        if(root == null)
+            return null;
+        else if(data < root.data)
+            root.left = deleteRec(root.left,data);
+        else if(data > root.data)
+            root.right = deleteRec(root.right,data);
+        else{
+            if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+            // node with two children: Get the inorder successor (smallest
+            // in the right subtree)
+            root.data = minValue(root.right);
+
+            // Delete the inorder successor
+            root.right = deleteRec(root.right, root.data);
+        }
+        return root;
+    }
+
+    int minValue(Node root)
+    {
+        int minv = root.data;
+        while (root.left != null)
+        {
+            minv = root.left.data;
+            root = root.left;
+        }
+        return minv;
+    }
+
     Node search(Node root, int key){
         if(root == null || root.data == key)
             return  root;
@@ -54,7 +92,6 @@ public class BinarySearchTree {
         bst.insert(6);
         bst.insert(3);
         bst.insert(2);
-        bst.insert(1);
         bst.insert(7);
         bst.insert(8);
 
@@ -64,5 +101,9 @@ public class BinarySearchTree {
             System.out.println("Element found");
         else
             System.out.println("Element not found");
+
+        bst.delete(5);
+        bst.inorderTraversal(bst.root);
+
     }
 }
