74a9f4be56467a0491933a8c9fe970fb9a5a1eac
Sibi Narayanan <sibnaray@users.noreply.github.com>
2020-01-17 21:54:05 -0800

    Create CoinChange_BU_Tabulation.java

---
+++ src/dynamicprograms/unboundedknapsack/CoinChange_BU_Tabulation.java
@@ -0,0 +1,26 @@
+package dynamicprograms.unboundedknapsack;
+
+public class CoinChange_BU_Tabulation {
+    public static void main(String[] args) {
+        int[] denominations = {1, 2, 3};
+        System.out.println(countChange(denominations, 5));
+    }
+    static int countChange(int[] denominations, int amount){
+        if(denominations.length==0 || amount == 0)
+            return 0;
+        int[][] dp = new int[denominations.length][amount+1];
+
+        for(int i=0 ; i<denominations.length; i++)
+            dp[i][0] = 1;
+        for(int i=0; i<denominations.length; i++){
+            for(int a=1; a<=amount; a++){
+                if(denominations[i] <= a){
+                    dp[i][a] = dp[i][a] + dp[i][a-denominations[i]];
+                }
+                if(i>0)
+                    dp[i][a] = dp[i][a] + dp[i-1][a];
+            }
+        }
+        return dp[denominations.length-1][amount];
+    }
+}
