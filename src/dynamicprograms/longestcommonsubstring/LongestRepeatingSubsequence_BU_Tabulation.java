50cfe8786f22d7b87177797953fdb0bbaa18d1a6
Sibi Narayanan <sibnaray@users.noreply.github.com>
2020-03-15 11:10:04 -0700

    Create LongestRepeatingSubsequence_BU_Tabulation.java

---
+++ src/dynamicprograms/longestcommonsubstring/LongestRepeatingSubsequence_BU_Tabulation.java
@@ -0,0 +1,26 @@
+package dynamicprograms.longestcommonsubstring;
+
+public class LongestRepeatingSubsequence_BU_Tabulation {
+    public static void main(String[] args) {
+        LongestRepeatingSubsequence_BU_Tabulation lrs = new LongestRepeatingSubsequence_BU_Tabulation();
+        System.out.println(lrs.findLRSLength("tomorrow"));
+        System.out.println(lrs.findLRSLength("aabdbcec"));
+        System.out.println(lrs.findLRSLength("fmff"));
+    }
+
+    int findLRSLength(String str){
+        int[][] dp = new int[str.length()+1][str.length()+1];
+        int maxLength=0;
+
+        for(int i=1;i<=str.length();i++){
+            for(int j=1;j<=str.length();j++){
+                if(i != j && str.charAt(i-1)==str.charAt(j-1))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                maxLength=Math.max(maxLength,dp[i][j]);
+            }
+        }
+        return maxLength;
+    }
+}
